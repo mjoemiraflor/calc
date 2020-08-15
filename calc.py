02efc186b31fcabe1a47177095f1f851ef5078fa
MICHAEL JOE E MIRAFLOR <miraflor.mj217@gmail.com>
2020-08-15 14:14:19 +0800

    display result based on selected operation

--- calc.py
+++ calc.py
@@ -6,5 +6,11 @@ def substract(num1, num2)
 
 inpupt1=int (input ('Enter the 1st num: '))
 inpupt2=int (input ('Enter the 2nd num: '))
+operation= inout('select operation (+,-):')
 
+if operation = '+':
 print('Result:', add(input1, input2))
+elif operation = '-':
+print('Result:', substract(input1, input2))
+else
+print('Invalid operation')
