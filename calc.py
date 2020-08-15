7c7771cf9961e5062dd77a10d4de45fb5f069b88
MICHAEL JOE E MIRAFLOR <miraflor.mj217@gmail.com>
2020-08-15 13:14:42 +0800

    display result

--- calc.py
+++ calc.py
@@ -4,4 +4,5 @@ def add(num1,num2):
 inpupt1=int (input ('Enter the 1st num: '))
 inpupt2=int (input ('Enter the 2nd num: '))
 
-print('Result:', add(input1, input2))
+sum=input1+input2
+print('Result:', sum)
